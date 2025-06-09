4278890049ae68cf4af3b70fa0fd35f41dd8768f
Ondrej Trtik <trtik.o@gmail.com>
2025-06-09 12:56:38 +0200

    final

--- hlavni/urls.py
+++ hlavni/urls.py
@@ -3,7 +3,7 @@ from . import views
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='staticka.html'), name='home'),  # ← přidáno pro /
+    path('', TemplateView.as_view(template_name='staticka.html'), name='home'),
     path('zobraz/', views.zobraz_parametr, name='zobraz_parametr'),
     path('kategorie/', views.vyber_kategorii, name='vyber_kategorie'),
     path('zpetna_vazba/', views.zpetna_vazba, name='zpetna_vazba'),
--- mujprojekt/settings.py
+++ mujprojekt/settings.py
@@ -14,11 +14,11 @@ INSTALLED_APPS = [
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',  # ← toto přidat
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',  # ← toto přidat
-    'django.contrib.messages.middleware.MessageMiddleware',  # ← toto přidat
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
